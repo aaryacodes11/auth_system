4b8259d035048fee5e35123d9c2a49f94f2bd4a1
Aarya <aarya.codes11@gmail.com>
2026-09-10 12:49:07 +0530

    Point frontend to deployed backend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -10,7 +10,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      const res = await fetch('http://localhost:5001/api/login', {
+      const res = await fetch('http://auth-system-twnl.onrender.com/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -12,7 +12,7 @@ function Profile() {
       setError('You must be logged in to view this page')
       return
     }
-    fetch('http://localhost:5001/api/profile', {
+    fetch('http://auth-system-twnl.onrender.com/api/profile', {
       headers: { Authorization: `Bearer ${token}` },
     })
       .then((res) => {
